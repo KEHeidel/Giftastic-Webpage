eae12071feab8351188da68a6fd0dfe48fb0ea36
Kortnie Heidel <kortnie.evans@gmail.com>
2020-03-18 14:23:03 -0400

    adds function for animating gif

--- javascript.js
+++ javascript.js
@@ -110,5 +110,17 @@ $(document).ready(function() {
             }
           });
         }
+        // function for animating gif
+        function gifPlay() {
+          var state = $(this).attr("data-state");
+          if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
+        }
+      
 
   });
