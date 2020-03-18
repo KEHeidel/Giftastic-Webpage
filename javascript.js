74f82fc515cbfe8484ea299eb74be2ed0e5b0342
Kortnie Heidel <kortnie.evans@gmail.com>
2020-03-18 14:18:37 -0400

    adds variable for game array

---
+++ javascript.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    // variable for the array of games and user input
+    var games = [
+      "Final Fantasy XIV",
+      "World of Warcraft",
+      "Diablo III",
+      "Horizon: Zero Dawn",
+      "Fallout 4",
+      "Overwatch",
+      "God of War",
+      "League of Legends",
+      "The Witcher 3",
+      "Destiny 2"
+    ];
+  
+    var searchinfo = "";
+  
+
+  });
