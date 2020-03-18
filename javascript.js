b2b6c967a7d1f708f460c27e83caba5ecab5c6ba
Kortnie Heidel <kortnie.evans@gmail.com>
2020-03-18 14:19:31 -0400

    adds variable for API

--- javascript.js
+++ javascript.js
@@ -15,5 +15,12 @@ $(document).ready(function() {
   
     var searchinfo = "";
   
+    // variables for pulling info from API
+    var apikey = "tvw4dJZXleyecwz1WsXqH65PK61XKnHD";
+    var apilimit = 10;
+    var apioffset = 0;
+    var apirating = "PG";
+    var apilang = "en";
+  
 
   });
